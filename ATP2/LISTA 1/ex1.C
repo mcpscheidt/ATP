264914f97c9ae6e3d3e5868eed4df3c73dd9aa73
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-07-31 12:53:05 -0300

    Update and rename vetores to ex1.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

--- ATP2/LISTA 1/ex1.C
+++ ATP2/LISTA 1/ex1.C
@@ -2,9 +2,9 @@
 #include <string.h>
 
 int main() {
-	int nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int nums[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         printf("resultados: ");
-	for(int i = 0; i < 10; i++) {
+	for(int i = 0; i < 9; i++) {
 		printf("%d\n", nums[i]);
 	}
 
