ba7cc0935cd2c2471ca47147216e47ac0f37e110
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-01 09:21:17 -0300

    Create ex3.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 1/ex3.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int nums[7] = {2, 3, 5, 7, 11, 13, 17};
+
+    printf("Primeiros 7 numeros primos: ");
+    for (int i = 0; i < 7; i++){
+        printf("%d ", nums[i]);
+    }
+
+
+
+return 0;
+}
