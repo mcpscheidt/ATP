cb66f168839efec6f76f5a980fd06d5331c89aaf
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-07 10:00:30 -0300

    Create EX 19(DEFINE).C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 1/EX 19(DEFINE).C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define array_size 8
+float media_elementos(int *nums){
+    float  soma = 0;
+    for(int i = 0; i < array_size; i++){
+        soma  += *(nums+i);
+    }
+
+    float media = soma/array_size;
+
+
+return media;
+}
+
+
+
+int main(){
+
+    int nums[array_size] = {1, 2, 3, 4, 86, 6, 7, 8};
+        printf("Resultado: %f", media_elementos(nums));
+
+
+return 0;
+}
+
+
