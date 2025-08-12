ab38ab8b211fff741ec4042bf9899f049e3deed4
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-11 22:00:32 -0300

    Create EX 23.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 1/EX 23.C
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_palindrome(int num) {
+    if (num < 0) {
+        return false;
+    }
+
+    int reverso = 0;
+    int original = num;
+
+    while (num != 0) {
+        int digito = num % 10;
+        reverso = reverso * 10 + digito;
+        num = num / 10;
+    }
+
+    if (reverso == original) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    int num;
+
+    printf("digite um numero: ");
+    scanf("%d", &num);
+
+    bool teste = is_palindrome(num);
+
+    if (teste == true) {
+        printf("eh palindromo\n");
+    } else {
+        printf("nao eh palindromo\n");
+    }
+
+    return 0;
+}
+
+
