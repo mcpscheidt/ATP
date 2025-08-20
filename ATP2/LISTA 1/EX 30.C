815eef22a5eac249c509211908058bbcd9cac2dd
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-20 13:28:05 -0300

    Update EX 30.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

--- ATP2/LISTA 1/EX 30.C
+++ ATP2/LISTA 1/EX 30.C
@@ -5,23 +5,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define NUM_STRINGS 3
-#define BUFFER_SIZE 256
+#define num_strings 3
+#define buffer_size 50
 
 int main() {
-    char *array[NUM_STRINGS];
-    char buffer[BUFFER_SIZE];
+    char *array[num_strings];
+    char buffer[buffer_size];
 
-    for (int i = 0; i < NUM_STRINGS; i++) {
-        fgets(buffer, BUFFER_SIZE, stdin);
+    for (int i = 0; i < num_strings; i++) {
+        fgets(buffer, buffer_size, stdin);
         buffer[strcspn(buffer, "\n")] = '\0';
 
         array[i] = (char *)malloc(strlen(buffer) + 1);
         strcpy(array[i], buffer);
     }
 
-    for (int i = 0; i < NUM_STRINGS - 1; i++) {
-        for (int j = i + 1; j < NUM_STRINGS; j++) {
+    for (int i = 0; i < num_strings - 1; i++) {
+        for (int j = i + 1; j < num_strings; j++) {
             if (strcmp(array[i], array[j]) > 0) {
                 char *temp = array[i];
                 array[i] = array[j];
@@ -31,7 +31,7 @@ int main() {
     }
 
     printf("\nStrings em ordem alfabética:\n");
-    for (int i = 0; i < NUM_STRINGS; i++) {
+    for (int i = 0; i < num_strings; i++) {
         printf("%s\n", array[i]);
          free(array[i]);
     }
