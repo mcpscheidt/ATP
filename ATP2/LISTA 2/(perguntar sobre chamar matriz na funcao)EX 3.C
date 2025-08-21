4b6fee6495513e8f49af20dd10e7f56668c9767a
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-21 09:53:08 -0300

    Create (perguntar sobre chamar matriz na funcao)EX 3.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 2/(perguntar sobre chamar matriz na funcao)EX 3.C
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include<stdbool.h>
+
+#define linhas 4
+#define colunas 4
+
+bool verificar_igualdade(int matriz[linhas][colunas],int matriz_secundaria[linhas][colunas]){
+
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            if(matriz[i][j] != matriz_secundaria[i][j]){
+                return false;
+            }
+        }
+    }
+return true;
+}
+
+
+
+int main() {
+int matriz[linhas][colunas] = { {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+int matriz_secundaria[linhas][colunas] = { {1, 2, 1, 1}, {1, 2, 1, 1}, {1, 1, 1, 1}};
+
+    if(verificar_igualdade(matriz, matriz_secundaria) == false){
+      printf("As matrizes sao diferentes");
+    }else printf("As matrizes sao iguais");
+
+
+
+return 0;
+}
