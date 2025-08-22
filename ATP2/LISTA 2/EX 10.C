91ac36995dffe1036929ccfb6acda90681fa4ee4
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-22 13:27:58 -0300

    Update EX 10.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

--- ATP2/LISTA 2/EX 10.C
+++ ATP2/LISTA 2/EX 10.C
@@ -1,1 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define linhas 4
+#define colunas 4
+
+int main() {
+int matriz[linhas][colunas] = { {1, 1, 1, 4}, {9, 2, 1, 5}, {1, 1, 1, 6},{8, 0, 1, 6}};
+int menor = matriz[0][0];
+
+for( int i = 0; i < linhas; i++)
+{
+    for(int z = 0 ; z < colunas; z++){
+        if(matriz[i][z] < menor){
+            menor = matriz[i][z];
+        }
+    }
+}
+
+for (int i = 0; i < linhas; i++)
+{
+    for (int j = 0; j < colunas; j++)
+    {
+        printf("%d ", matriz[i][j]);
+    }
+
+    printf("\n");
+}
+
+
+printf("------------------  \n");
+
+printf("O menor numero desta matriz eh: %d", menor);
+
+return 0;
+}
+
 
