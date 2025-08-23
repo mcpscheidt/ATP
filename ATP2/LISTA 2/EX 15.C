5abb8ddc3ee5a38c70336aee9f4fa690c7d49ec0
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-23 12:43:46 -0300

    Create EX 15.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 2/EX 15.C
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define linhas 3
+#define colunas 3
+
+int main() {
+int matriz[linhas][colunas] = { {1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+int matriz_2[linhas][colunas];
+int aux = linhas - 1;
+printf("Matriz A:\n");
+for (int i = 0; i < linhas; i++)
+{
+    for (int j = 0; j < colunas; j++)
+    {
+        printf("%d ", matriz[i][j]);
+    }
+
+    printf("\n");
+}
+
+
+
+for( int i = 0; i < linhas; i++)
+{
+    for(int z = 0 ; z < colunas; z++){
+    matriz_2[i][z] = matriz[i][aux];
+    aux--;
+    }
+}
+
+printf("Matriz A com linhas invertidas:\n");
+for (int i = 0; i < linhas; i++)
+{
+    for (int j = 0; j < colunas; j++)
+    {
+        printf("%d ", matriz_2[i][j]);
+    }
+
+    printf("\n");
+}
+
+
+
+return 0;
+}
+
