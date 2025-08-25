81e3d6f0e3d28ec37348264ecb5b3d86686f8ffd
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-25 11:02:54 -0300

    Create EX 27.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 2/EX 27.C
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define L 3
+#define C 3
+
+int main() {
+int matriz[L][C] = {{1,1,1},{1,1,1},{1,1,1},};
+int num;
+
+printf("Matriz A: \n");
+
+for (int i = 0; i < L; i++)
+{
+    for (int j = 0; j < C; j++)
+    {
+        printf("%d ", matriz[i][j]);
+    }
+
+    printf("\n");
+}
+
+printf("Escolha um numero para multiplicar a matriz ");
+scanf("%d", &num);
+
+
+for(int i = 0; i < L; i++){
+    for(int j = 0; j < C; j++){
+
+            matriz[i][j] = matriz[i][j]*num;
+
+    }
+}
+
+printf("Matriz A multiplicada por %d: \n", num);
+
+for (int i = 0; i < L; i++)
+{
+    for (int j = 0; j < C; j++)
+    {
+        printf("%d ", matriz[i][j]);
+    }
+
+    printf("\n");
+}
+
+
+return 0;
+}
+
+
+
