5811191928e335ac0db0097387ccd5ee320f79a3
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-08-27 22:07:27 -0300

    Create EX 40.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 2/EX 40.C
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define L 5
+#define C 5
+
+int main() {
+int matriz[L][C] = {{1, 6, 11, 16, 21}, {2, 7, 12, 17, 22}, {3, 8, 13, 18, 23}, {4, 9, 14, 19, 24}, {5, 10, 15, 20, 25}};
+int matriz_2[L][C];
+
+
+//for(int i = 0; i < L; i++){
+//    for(int j = 0; j < C; j++){
+//        matriz[i][j] = rand()%101;
+//    }
+//}
+
+printf("Matriz A: \n");
+for(int i = 0; i < L; i++){
+        for(int j = 0; j < C; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+}
+
+int j = C -1;
+for (int i = 0; i < C; i++){
+    for (int z = 0; z < L; z++){
+        matriz_2[z][L -1 -i] = matriz[i][z];
+        }
+    }
+
+
+printf("Matriz A rotacionada no sentido horario: \n");
+for(int i = 0; i < L; i++){
+        for(int j = 0; j < C; j++)
+        {
+            printf("%d ", matriz_2[i][j]);
+        }
+        printf("\n");
+}
+return 0;
+}
+
+
+
