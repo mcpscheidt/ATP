e95e656c93e543a2279aff437f4dc46044d01a4d
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-09-07 16:11:46 -0300

    Update EX 32.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

--- ATP2/LISTA 1/( colocar free) EX 32.C
+++ ATP2/LISTA 1/( colocar free) EX 32.C
@@ -1,30 +1,38 @@
+```c
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define array_size 5
 #define word_size 1024
 
 
 int main() {
-    char* array[array_size];
-    int tamanho_palavra[array_size];
+    char* array[array_size];
+    int tamanho_palavra[array_size];
 
 
-        for(int i = 0; i < array_size; i++){
-            array[i] = malloc(word_size);
-            fgets(array[i], word_size, stdin);
+        for(int i = 0; i < array_size; i++){
+            array[i] = malloc(word_size);
+            fgets(array[i], word_size, stdin);
 
-        }
+        }
 
-        int indice_maior_palavra = 0;
+        int indice_maior_palavra = 0;
 
-    for(int i = 1; i < array_size; i++){
-        if(strlen(array[i]) > strlen(array[indice_maior_palavra])){
-            indice_maior_palavra = i;
-        }
+    for(int i = 1; i < array_size; i++){
+        if(strlen(array[i]) > strlen(array[indice_maior_palavra])){
+            indice_maior_palavra = i;
+        }
+    }
+
+    printf("A palavra mais comprida eh: %s\n", array[indice_maior_palavra]);
+
+    for (int i = 0; i < array_size; i++) {
+        free(array[i]);
     }
-
-    printf("A palavra mais comprida eh: %s\n", array[indice_maior_palavra]);
-    return 0;
+    
+    return 0;
 }
+```
