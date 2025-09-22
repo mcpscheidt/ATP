0fc73374a25063900efa92054fcd2ce41d9485e1
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-09-22 16:35:39 -0300

    Create SelectionSort.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/Algoritmos_de_ordenacao/SelectionSort.C
@@ -0,0 +1,1 @@
+//A ideia aqui eh achar o menor valor do array e coloca-lo na posicao 0. Para isso vamos usar uma funcao para achar o menor e outra para trocar o menor atual pelo primeiro e assim por diante
