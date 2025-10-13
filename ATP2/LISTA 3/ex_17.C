6d06ea775b9934b3ef41d3efdfb1d7e03f4aa519
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-10-13 14:37:13 -0300

    Create ex_17.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 3/ex_17.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct{
+
+    char nome[50];
+    char diretor[50];
+    int ano_lancamento;
+
+}Filme;
+
+int main(){
+    Filme* filme1;
+
+     filme1 = (Filme*) malloc(sizeof(Filme));
+
+    strcpy(filme1->nome, "Star Wars");
+
+    strcpy(filme1->diretor, "George Lucas");
+
+    filme1->ano_lancamento = 1964;
+
+    printf("Filme: %s\n", filme1->nome);
+
+    printf("Diretor: %s\n", filme1->diretor);
+
+    printf("Ano de lancamento: %d\n", filme1->ano_lancamento);
+
+    return 0;
+}
