694ca4e214545a9354b74188c6644a3c7b816173
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-10-14 00:17:03 -0300

    Create ex_23.C
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

---
+++ ATP2/LISTA 3/ex_23.C
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <string.h>
+
+
+typedef enum {
+   JANEIRO =1,
+   FEVEREIRO,
+   MARCO,
+   ABRIL,
+   MAIO,
+   JUNHO,
+   JULHO,
+   AGOSTO,
+   SETEMBRO,
+   OUTUBRO,
+   NOVEMBRO,
+   DEZEMBRO,
+
+} Calendario;
+
+
+
+int main() {
+
+
+
+   printf("numeros: %d %d %d %d %d %d %d %d %d %d %d %d ", JANEIRO,
+   FEVEREIRO,
+   MARCO,
+   ABRIL,
+   MAIO,
+   JUNHO,
+   JULHO,
+   AGOSTO,
+   SETEMBRO,
+   OUTUBRO,
+   NOVEMBRO,
+   DEZEMBRO);
+
+    return 0;
+}
+
