762c5339dcd57987b2911f0df6bbef7d2ccc39c0
Matheus Pscheidt <mcpscheidt@gmail.com>
2025-10-20 15:13:21 -0300

    Add file handling to write 'Hello, World!' to text.txt
    Signed-off-by: Matheus Pscheidt <mcpscheidt@gmail.com>

--- ATP2/LISTA 4/ex1.C
+++ ATP2/LISTA 4/ex1.C
@@ -1,1 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+FILE *fp;
+
+    fp = fopen("text.txt", "w");
+    fprintf(fp, "Hello, World!");
+    fclose(fp);
+
+
+return 0;
+}
 
